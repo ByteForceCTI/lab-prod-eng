1ba8ea7be1b4fa15029a25bc97a17529850c16e8
alexia-maria <115625535+alexia-maria@users.noreply.github.com>
2025-03-17 09:13:33 +0000

    Created userService and userController

--- src/main/java/ro/unibuc/hello/dto/UserDto.java
+++ src/main/java/ro/unibuc/hello/dto/UserDto.java
@@ -1,12 +1,11 @@
 package ro.unibuc.hello.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDto {
     private String username;
     private String name;
     private String bio;
     private String profilePicture;
-
-    public UserDto(){
-        
-    }
 }
