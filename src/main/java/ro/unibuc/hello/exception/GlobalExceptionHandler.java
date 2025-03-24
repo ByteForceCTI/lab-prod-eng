650753f73c789d797897a5c781d811f955bcc976
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2025-03-24 09:17:09 +0000

    update request exception handler to support 404

--- src/main/java/ro/unibuc/hello/exception/GlobalExceptionHandler.java
+++ src/main/java/ro/unibuc/hello/exception/GlobalExceptionHandler.java
@@ -13,13 +13,19 @@ public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    // This method catches all exceptions
+    // map EntityNotFoundException to HTTP 404 code 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        logger.error("Entity not found: {} - Request: {}", ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // map any untreated exception to error 500
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleAllExceptions(Exception ex, WebRequest request) {
         logger.error("Unhandled exception occurred while processing request: {}",
                      request.getDescription(false), ex);
-        // Customize the error response as needed.
+        // custom error message for generic exception
         return new ResponseEntity<>("An internal error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
-
